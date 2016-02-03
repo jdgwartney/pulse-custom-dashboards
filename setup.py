a0aae6a7b5dab1c2d397ad98a71d3b5fbab86462
Gwartney <davidg@boundary.com>
2016-02-03 11:31:57 -0800

    Stub package for custom dashboards

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='tspdash',
+    version='0.0.1',
+    url="https://github.com/boundary/pulse-custom-dashboards",
+    author='David Gwartney',
+    author_email='david_gwartney@bmc.com',
+    packages=['tspdash', ],
+#    entry_points={
+#        'console_scripts': [
+#            'actionhandler = boundary.webhook_handler:main',
+#        ],
+#    },
+#    scripts=[
+#        'tsp-cli-env.sh',
+#   ],
+#    package_data={'boundary': ['templates/*']},
+    license='Apache 2',
+    description='Python Bindings for the TrueSight Pulse REST APIs',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
